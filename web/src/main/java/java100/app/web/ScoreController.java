3572f8158a34df6a9a4a2e7ae4e0a7f13713716b
dumnight <dumnight@gmail.com>
2018-01-22 18:12:43 +0900

    233

--- web/src/main/java/java100/app/web/ScoreController.java
+++ web/src/main/java/java100/app/web/ScoreController.java
@@ -17,7 +17,7 @@ import java100.app.service.ScoreService;
 public class ScoreController {
     
     @Autowired ScoreService scoreService;
-    
+ 
     @RequestMapping("update")
     public String update(Score score) throws Exception {
         
@@ -26,7 +26,6 @@ public class ScoreController {
         // 프론트 컨트롤러가 실행할 JSP URL을 등록한다.
         return "redirect:list"; 
     }
-   
     
     @RequestMapping("add")
     public String add(Score score) throws Exception {
@@ -34,6 +33,15 @@ public class ScoreController {
         scoreService.add(score);
         return "redirect:list";
     }
+    
+    
+    @RequestMapping("{no}")
+    public String view(@PathVariable int no, Model model) throws Exception {
+        
+        model.addAttribute("score", scoreService.get(no));
+        return "score/view";        
+    }
+
     
     
     @RequestMapping("list")
