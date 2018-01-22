c39add889f9af5ba4bad950d572a9683a7c0a8e8
junyub301 <junyub0301@gmail.com>
2018-01-22 18:11:53 +0900

    ㅋㅋ

--- web/src/main/java/java100/app/web/ScoreController.java
+++ web/src/main/java/java100/app/web/ScoreController.java
@@ -2,6 +2,8 @@ package java100.app.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java100.app.domain.Score;
