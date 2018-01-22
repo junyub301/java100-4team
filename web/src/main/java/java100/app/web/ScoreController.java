254c58abf17cee6c4e828bb62b48b43276e8d44f
seojung0 <seojung_7@naver.com>
2018-01-22 18:08:45 +0900

    gg

--- web/src/main/java/java100/app/web/ScoreController.java
+++ web/src/main/java/java100/app/web/ScoreController.java
@@ -1,13 +1,10 @@
 package java100.app.web;
 
-import java.util.HashMap;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java100.app.domain.Score;
 import java100.app.service.ScoreService;
@@ -18,6 +15,21 @@ public class ScoreController {
     
     @Autowired ScoreService scoreService;
     
+    @RequestMapping("update")
+    public String update(Score score) throws Exception {
+        
+        scoreService.update(score);
+        
+        // 프론트 컨트롤러가 실행할 JSP URL을 등록한다.
+        return "redirect:list"; 
+    }
+    
+    @RequestMapping("add")
+    public String add(Score score) throws Exception {
+        
+        scoreService.add(score);
+        return "redirect:list";
+    }
     
     
     @RequestMapping("{no}")
