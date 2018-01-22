df3fc44b9effb8cc706d8d5d78db24c4d61b1f91
dumnight <dumnight@gmail.com>
2018-01-22 18:03:06 +0900

    .

--- web/src/main/java/java100/app/web/ScoreController.java
+++ web/src/main/java/java100/app/web/ScoreController.java
@@ -18,7 +18,46 @@ public class ScoreController {
     
     @Autowired ScoreService scoreService;
     
-    
+    @RequestMapping("list")
+    public String list(
+            @RequestParam(value="pn", defaultValue="1") int pageNo,
+            @RequestParam(value="ps", defaultValue="5") int pageSize,
+            @RequestParam(value="words", required=false) String[] words,
+            @RequestParam(value="oc", required=false) String orderColumn,
+            @RequestParam(value="al", required=false) String align,
+            Model model) throws Exception {
+
+        // UI 제어와 관련된 코드는 이렇게 페이지 컨트롤러에 두어야 한다.
+        //
+        if (pageNo < 1) {
+            pageNo = 1;
+        }
+        
+        if (pageSize < 5 || pageSize > 15) {
+            pageSize = 5;
+        }
+        
+        HashMap<String,Object> options = new HashMap<>();
+        
+        if (words != null && words[0].length() > 0) {
+            options.put("words", words);
+        }
+        options.put("orderColumn", orderColumn);
+        options.put("align", align);
+        
+        int totalCount = scoreService.getTotalCount();
+        int lastPageNo = totalCount / pageSize;
+        if ((totalCount % pageSize) > 0) {
+            lastPageNo++;
+        }
+        
+        // view 컴포넌트가 사용할 값을 Model에 담는다.
+        model.addAttribute("pageNo", pageNo);
+        model.addAttribute("lastPageNo", lastPageNo);
+        model.addAttribute("list", scoreService.list(pageNo, pageSize, options));
+        
+        return "score/list";
+    }
 }
 
 
