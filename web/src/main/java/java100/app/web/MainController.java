0a265fe79dd35048bc271c8d9f99cb39a03d5b50
junyub301 <junyub0301@gmail.com>
2018-01-26 10:55:50 +0900

    '메인추가'

---
+++ web/src/main/java/java100/app/web/MainController.java
@@ -0,0 +1,24 @@
+package java100.app.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/main")
+public class MainController {
+    
+    @RequestMapping("main")
+    public String form() throws Exception {
+        return "main/main";
+    }
+     
+    
+}
+
+
+
+
+
+
+
+
