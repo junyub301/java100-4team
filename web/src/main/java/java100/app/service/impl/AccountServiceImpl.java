bc950ad63831d654baf5a8e4c2f8173e8b135a10
seojung0 <seojung_7@naver.com>
2018-01-26 11:40:42 +0900

    로그인 ver1.0 accountservice ,accountlmpl logincontroller 추가 accountDao.xml변경

---
+++ web/src/main/java/java100/app/service/impl/AccountServiceImpl.java
@@ -0,0 +1,27 @@
+package java100.app.service.impl;
+
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java100.app.dao.AccountDao;
+import java100.app.domain.Account;
+import java100.app.service.AccountService;
+
+@Service
+public class AccountServiceImpl implements AccountService {
+    @Autowired
+    AccountDao accountDao;
+    @Override
+    public Account get(String accountName, String password) {
+        HashMap<String,Object> params = new HashMap<>();
+        params.put("accountName", accountName);
+        params.put("password", password);
+        
+        return accountDao.findByaccountNameAndPassword(params);
+    }
+
+   
+
+}
