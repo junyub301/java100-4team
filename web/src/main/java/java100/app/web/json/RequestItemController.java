e91e3b66228da63733238818ebc37293e9291458
dazzul94 <dazzul94@naver.com>
2018-03-08 17:22:14 +0900

    reqeust3

--- web/src/main/java/java100/app/web/json/RequestItemController.java
+++ web/src/main/java/java100/app/web/json/RequestItemController.java
@@ -6,9 +6,11 @@ import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java100.app.domain.Message;
 import java100.app.domain.RequestItem;
 import java100.app.domain.User;
 import java100.app.service.RequestItemService;
@@ -35,8 +37,30 @@ public class RequestItemController {
         requestItem.setUserNo(user.getUserNo());
         requestItemService.add(requestItem);
         HashMap<String, Object> result = new HashMap<>();
-        result.put("stauts", "success");
+        result.put("status", "success");
         return result;
     }
-    
+    @RequestMapping("update")
+    public Object update(RequestItem requestItem) throws Exception  {
+        requestItemService.update(requestItem);
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("status", "success");
+        return result;
+    }
+
+    @RequestMapping("delete")
+    public Object delete(int no) throws Exception  {
+        requestItemService.delete(no);
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("status", "success");
+
+        return result;
+    }
+    @RequestMapping("{no}")
+    public Object view(@PathVariable int no) throws Exception {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("requestItem", requestItemService.get(no));
+
+        return result;
+    }
 }
