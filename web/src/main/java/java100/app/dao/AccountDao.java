29edfece323e42783ee182a01d0350788f543bf1
junyub301 <junyub0301@gmail.com>
2018-03-08 10:20:19 +0900

    마이페이지 수정

---
+++ web/src/main/java/java100/app/dao/AccountDao.java
@@ -0,0 +1,35 @@
+package java100.app.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import java100.app.domain.Account;
+
+public interface AccountDao {
+    List<Account> list();
+    int insert(Account account);
+    Account findByEmail(String email);
+    Account findByNo(int no);
+    int update(Account account);
+    int update2(Account account);
+    int delete(int no);
+    Account findByaccountNameAndPassword(Map<String,Object> params);
+    int checkId(String id);
+    int checkEmail(String email);
+    int checkPassword(Map<String,Object> params);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
